a04bcf64aa5cdc6201566b848ef86d0cacd40db3
george <geobyt@gmail.com>
2015-01-19 20:46:52 -0800

    Initial Commit

---
+++ gatherall2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gatherall2
+//
+//  Created by George on 1/19/15.
+//  Copyright (c) 2015 George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
